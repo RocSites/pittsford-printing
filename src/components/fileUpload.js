cd07c4e2040e638c16c702f5d996d755c05350a4
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-12-14 16:22:35 -0500

    added loading treatment for file upload

--- src/components/fileUpload.js
+++ src/components/fileUpload.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react';
-// import fetch from 'fetch';
-import AWS from 'aws-sdk';
+import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field } from 'formik';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import CheckIcon from '@mui/icons-material/Check';
 
 
 
 const FileUpload = (props) => {
     const [file, setFile] = useState(null);
-    const [uploadProgress, setUploadProgress] = useState(null);
+    const [uploadProgress, setUploadProgress] = useState(false);
     const [showUploadComplete, setShowUploadComplete] = useState(false);
 
 
+
     const uploadFile = async () => {
+        setUploadProgress(true)
         const resp = await fetch(`https://u6gk632v5cmbxsom35w2eykmoq0xdraf.lambda-url.us-east-1.on.aws/?file_name=${file.name}&bucket=${props.bucket}`)
-        const body = await resp.json(); 
+        const body = await resp.json();
         console.log(props.bucket)
 
         const uploadResult = await fetch(body.url, {
             method: "PUT",
-            headers:{
+            headers: {
                 'Content-Type': file.type
             },
             body: file,
-          });
-          setShowUploadComplete(true)
-          props.setFileUploaded(true)
-          props.setS3Path(`${body.prefix}/${body.fileName}`)
+        });
+        setUploadProgress(false)
+        setShowUploadComplete(true)
+        props.setFileUploaded(true)
+        props.setS3Path(`${body.prefix}/${body.fileName}`)
     };
 
     const handleFileChange = (e) => {
@@ -40,13 +45,13 @@ const FileUpload = (props) => {
     return (
         <div>
             <input type="file" onChange={handleFileChange} />
-            {file && <Field type="hidden" name="file_name" value={props.s3Path}/> }
-            {file && <Field type="hidden" name="file_type" value={file.type}/> }
-            <button type="button" disabled={file === null } onClick={uploadFile}>Upload</button>
+            {file && <Field type="hidden" name="file_name" value={props.s3Path} />}
+            {file && <Field type="hidden" name="file_type" value={file.type} />}
+            <button type="button" disabled={file === null} onClick={uploadFile}>Upload</button>
             {uploadProgress ?
-                <p>{uploadProgress}</p> : null
+                <CircularProgress style={{marginLeft: "10px", marginBottom: "-13px", color: "#03178e"}} value={uploadProgress} /> : null
             }
-            {showUploadComplete === true ? <p>Your upload is complete!</p> : null}
+            {showUploadComplete === true ? <CheckIcon style={{backgroundColor: "#5bd75b", borderRadius: "15px", marginLeft: "10px", marginBottom: "-7px"}}/> : null}
 
         </div>
     );
