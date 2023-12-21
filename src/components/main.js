8167581416eab80b3a5daa32b8fc6ed69c533f15
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-12-21 14:23:44 -0500

    more space

--- src/components/main.js
+++ src/components/main.js
@@ -579,7 +579,7 @@ const withStyles = makeStyles(() => ({
             justifyContent: "center",
         },
         "@media(max-width:600px)": {
-            paddingTop: "30%",
+            paddingTop: "50%",
             backgroundSize: "cover",
             backgroundPositionY: "60px"
         }
