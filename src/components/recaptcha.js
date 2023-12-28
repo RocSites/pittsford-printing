bb81feaa1cef45a25fc9b1990a9b7f9df8adcd2b
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-12-28 16:19:38 -0500

    added dependency

--- src/components/recaptcha.js
+++ src/components/recaptcha.js
@@ -1,26 +1,29 @@
+import React, { useEffect, useCallback } from 'react';
+
+
 import {
     GoogleReCaptchaProvider,
     useGoogleReCaptcha
-  } from 'react-google-recaptcha-v3';
-  
-  const Recaptcha = () => {
-    const { executeRecaptcha } = useGoogleReCaptcha();
-  
-    const handleReCaptchaVerify = useCallback(async () => {
-      if (!executeRecaptcha) {
-        console.log('Execute recaptcha not yet available');
-        return;
-      }
-  
-      const token = await executeRecaptcha('yourAction');
-    }, [executeRecaptcha]);
-  
-    useEffect(() => {
-      handleReCaptchaVerify();
-    }, [handleReCaptchaVerify]);
-  
-    return <button onClick={handleReCaptchaVerify}>Verify recaptcha</button>;
-  };
-  
+} from 'react-google-recaptcha-v3';
 
-  export default Recaptcha;
+const Recaptcha = () => {
+    const { executeRecaptcha } = useGoogleReCaptcha();
+
+    const handleReCaptchaVerify = useCallback(async () => {
+        if (!executeRecaptcha) {
+            console.log('Execute recaptcha not yet available');
+            return;
+        }
+
+        const token = await executeRecaptcha('yourAction');
+    }, [executeRecaptcha]);
+
+    useEffect(() => {
+        handleReCaptchaVerify();
+    }, [handleReCaptchaVerify]);
+
+    return <button onClick={handleReCaptchaVerify}>Verify recaptcha</button>;
+};
+
+
+export default Recaptcha;
