2a903058092bd4a5923318480edadb5f3e556fd3
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-02-28 11:47:10 -0500

    added paf approve page

---
+++ src/pages/paf-approve.js
@@ -0,0 +1,86 @@
+import React from "react"
+import Layout from "../components/layout"
+import {makeStyles} from '@material-ui/core/styles'
+import blackSwoosh from "../images/BackgroundSwooshBlack.png"
+
+
+const withStyles = makeStyles((theme) => ({
+  thankYouRoot:{
+    display: "flex",
+    backgroundImage: `url(${blackSwoosh})`,
+    backgroundSize: "cover",
+    flexDirection: "column",
+    backgroundRepeat: "no-repeat",
+    backgroundPositionY: "140px",
+    backgroundPositionX: "center",
+    backgroundColor: "black",
+    paddingTop: "20%",
+    paddingRight: "30px",
+    justifyContent: "flex-start",
+    height: "100vh",
+    padding: "20px",
+    "@media(max-width:1200px)": {
+        paddingTop: "30%",
+    },
+    "@media(max-width:800px)": {
+        paddingTop: "50%",
+        justifyContent: "center",
+    },
+    "@media(max-width:600px)": {
+        paddingTop: "60%",
+        backgroundSize: "cover",
+        backgroundPositionY: "60px"
+    }
+
+  },
+  thankYouMessage:{
+    display: "flex",
+    fontSize: "2.0rem",
+    fontWeight: "bold",
+    marginLeft:"auto",
+    marginRight: "auto",
+    color: "white",
+    lineHeight: "2.5rem",
+    textAlign: "center"
+  },
+  thankYouMessageItalics: {
+    display: "flex",
+    fontSize: "2.0rem",
+    fontWeight: "bold",
+    marginLeft:"auto",
+    marginRight: "auto",
+    color: "white",
+    lineHeight: "2.5rem",
+    textAlign: "center",
+    fontStyle: "italic"
+  },
+  thankYouMessageWrapper: {
+    display: "flex",
+    flexDirection: "column",  
+    justifyContent: "center",
+    marginLeft: "20px",
+    marginRight: "20px",
+    "@media(max-width: 600px)": {
+      marginTop: "200px",
+
+    }
+  }
+
+}));
+
+const PafApprove = () => {
+  const classes = withStyles();
+  return (
+  <Layout showContact={false}>
+    <div className={classes.thankYouRoot}>
+      <div className={classes.thankYouMessageWrapper}>
+        <p className={classes.thankYouMessage}>Your annual USPS PAF submission </p>
+        <p className={classes.thankYouMessage}>has been recieved and accepted. </p>
+        <p className={classes.thankYouMessageItalics}>Thank you!</p>
+      </div>
+    </div>
+  </Layout>
+  )
+}
+
+export default PafApprove
