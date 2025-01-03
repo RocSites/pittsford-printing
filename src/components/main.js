3bc298118dafa71e2be72bc283b892309a2ccec2
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2025-01-03 12:30:08 -0500

    removed holiday banner

--- src/components/main.js
+++ src/components/main.js
@@ -665,7 +665,7 @@ const withStyles = makeStyles(() => ({
 const Main = () => {
     const classes = withStyles();
 
-    const [modalOpen, setModalOpen] = useState(true);
+    const [modalOpen, setModalOpen] = useState(false);
     const handleClose = () => setModalOpen(false);
 
     const style = {
